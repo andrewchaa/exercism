7927cc94b6c85d2d8d46eebbe23e0087cb5ee488
Andrew Chaa <andrew.chaa@clear.bank>
2019-01-10 16:28:54 +0000

    initial

---
+++ hamming/Hamming.fs
@@ -0,0 +1,18 @@
+﻿module Hamming
+
+open System
+
+let distance (strand1: string) (strand2: string): int option = 
+    let compareChar (c1: char) (c2: char): int = 
+        if c1 = c2 then 0
+        else 1
+    
+    let compare ((cs1: list<char>), (cs2: list<char>)): int option =
+        match cs1, cs2 with
+        | ([], []) -> Some(0)
+        | (x::xs, y::ys) -> Some((compareChar x y) + (compare xs ys))
+        | _ -> Some(0)
+
+    if String.IsNullOrEmpty(strand1) && String.IsNullOrEmpty(strand2) then
+        Some(0)
+    else compare (Seq.toList(strand1), Seq.toList(strand2))
