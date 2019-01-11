8227ba94501bb08f8b1eafe67ff813ec1924209f
Andrew Chaa <andrew.chaa@clear.bank>
2019-01-11 16:43:01 +0000

    use pattern matching

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -10,9 +10,12 @@ let distance (strand1: string) (strand2: string): int option =
     let rec compare ((cs1: char list), (cs2: char list)): int =
         match cs1, cs2 with
         | (x::xs, y::ys) -> (compareChar x y) + (compare (xs, ys))
+        | ([], []) -> 0
         | _, _ -> 0
 
-    if String.IsNullOrEmpty strand1 && String.IsNullOrEmpty strand2 then Some(0)
-    else if strand1.Length <> strand2.Length then None
-    else Some(compare (Seq.toList(strand1), Seq.toList(strand2)))
 
+    match strand1, strand2 with
+    | (null, _) -> None
+    | (_, null) -> None
+    | (x, y) when x.Length <> y.Length -> None
+    | (x, y) -> Some(compare (Seq.toList(x), Seq.toList(y)))
