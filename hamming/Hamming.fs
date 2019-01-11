e7942abf93457011139acc6348ea5f22a81797b7
Andrew Chaa <andrew.chaa@clear.bank>
2019-01-11 14:13:40 +0000

    hamming done

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -7,12 +7,12 @@ let distance (strand1: string) (strand2: string): int option =
         if c1 = c2 then 0
         else 1
     
-    let compare ((cs1: list<char>), (cs2: list<char>)): int option =
+    let rec compare ((cs1: char list), (cs2: char list)): int =
         match cs1, cs2 with
-        | ([], []) -> Some(0)
-        | (x::xs, y::ys) -> Some((compareChar x y) + (compare xs ys))
-        | _ -> Some(0)
+        | (x::xs, y::ys) -> (compareChar x y) + (compare (xs, ys))
+        | _, _ -> 0
 
-    if String.IsNullOrEmpty(strand1) && String.IsNullOrEmpty(strand2) then
-        Some(0)
-    else compare (Seq.toList(strand1), Seq.toList(strand2))
+    if String.IsNullOrEmpty strand1 && String.IsNullOrEmpty strand2 then Some(0)
+    else if strand1.Length <> strand2.Length then None
+    else Some(compare (Seq.toList(strand1), Seq.toList(strand2)))
+
