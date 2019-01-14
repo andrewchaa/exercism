09f174d5f1bd02d071751f8797ba91741b83092d
Andrew Chaa <andrew.chaa@clear.bank>
2019-01-14 13:06:11 +0000

    robot

---
+++ robot-name/RobotName.fs
@@ -0,0 +1,33 @@
+﻿module RobotName
+
+open System
+
+type Robot(name) =
+    do  
+        Robot.ExistingNames = Robot.ExistingNames @ [name] |> ignore
+
+    static member ExistingNames:List<string> = []
+    member _this.Name = name
+
+
+let mkRobot() = 
+    let rand = new Random()
+    let rec generateName() : string =
+        let name =
+            [
+                yield! [1..2] |> List.map (fun _ -> ['A'..'Z'].[rand.Next(['A'..'Z'].Length)])
+                yield! [1..3] |> List.map (fun _ -> ['0'..'9'].[rand.Next(['0'..'9'].Length)])
+            ]
+            |> String.Concat
+        
+        if List.contains name Robot.ExistingNames then generateName()
+        else name
+
+    new Robot(generateName())
+
+let name (robot: Robot) = 
+    printf "%s" robot.Name
+    robot.Name
+
+let reset robot = mkRobot()
+
