86fca2287ac5cfbed7d843a8666f561736ed5cf3
Andrew Chaa <andrew.chaa@clear.bank>
2019-01-15 17:04:46 +0000

    isogram done

---
+++ isogram/Isogram.fs
@@ -0,0 +1,15 @@
+﻿module Isogram
+
+let isIsogram (str: string) = 
+    let rec count (c: char) (cs: char list) =
+        match c with
+        | ' ' | '-' -> 1
+        | _ ->  match cs with
+                | [] -> 0
+                | x::xs ->  if (x = c) then 1 + count c xs
+                            else count c xs
+    
+    str.ToLower() 
+    |> Seq.toList
+    |> fun xs -> List.forall (fun elem -> (count elem xs) = 1) xs
+
